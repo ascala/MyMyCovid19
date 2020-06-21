8f7a7035f340ab4bd2d1c8856b0a82daa2138010
ascala <antonio.scala@cnr.it>
2020-06-21 20:48:54 +0200

    Fixed the first figure with age cohorts and mobility

--- decoupledVScoupled.m
+++ decoupledVScoupled.m
@@ -13,9 +13,16 @@ figure(2)
 mobReg=["Friuli" "Emilia" "Marche" "Abruzzo" "Sicilia" "Toscana" "Lazio" "Lombardia" "Campania" "Molise" "Puglia" "Basilicata" "Trentino" "Calabria" "Veneto" "Piemonte" "Val d'Aosta" "Liguria" "Sardegna"	"Umbria"];
 %x=sum(Apre-diag(diag(Apre)));
 x=sum(Apre);
-[t s]=sort(x,'descend'); s=1:20;
+[t s]=sort(x,'descend'); 
+s=1:20;
 imagesc(log(Apre(s,s)))
 set(gca,'ytick',1:20,'yticklabel',mobReg(s))
 set(gca,'xtick',1:20,'xticklabel',mobReg(s),'xaxisLocation','top'); xtickangle(-90)
 
+figure(3)
+ageCoh=["00-04","05-09","10-14","15-19","20-24","25-29","30-34","35-39","40-44","45-49","50-54","55-59","60-64","65-69","70+"];
+imagesc(log(C)); % colorbar
+c=1:15;
+set(gca,'ytick',c,'yticklabel',ageCoh(c))
+set(gca,'xtick',c,'xticklabel',ageCoh(c),'xaxisLocation','top'); xtickangle(-90)
 
